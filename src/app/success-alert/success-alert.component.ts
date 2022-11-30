82bc6dc8ee63bfd1010f83355c895da6d63840dd
Sayak Saha <sayak.saha.fiem.ece19@teamfuture.in>
2022-11-30 16:59:59 +0530

    assignment 1 done

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'SuccessAlert',
+    templateUrl: './success-alert.component.html'
+})
+export class SuccessAlertComponent {
+    
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'WarningAlert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+
+}
